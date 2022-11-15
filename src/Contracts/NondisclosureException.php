bee67380b44a6d483570b103e22342c3023acb70
陈 <15690057962@163.com>
2022-11-15 09:55:14 +0800

    init

---
+++ src/Contracts/NondisclosureException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace HyperfApiResponder\Contracts;
+
+/**
+ * @method string getMessage()
+ * @method string getFile()
+ * @method int getLine()
+ * @method int getCode()
+ * @method array getTrace()
+ * @method string getTraceAsString()
+ */
+interface NondisclosureException
+{
+
+}
---
+++ src/ResponseError.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace HyperfApiResponder;
+
+use App\Exception\ValidateException;
+use Hyperf\HttpMessage\Stream\SwooleStream;
+use Hyperf\Utils\Codec\Json;
+use Psr\Http\Message\ResponseInterface;
+use HyperfApiResponder\Contracts\ServiceException;
+use HyperfApiResponder\Contracts\NondisclosureException;
+use HyperfApiResponder\Entity\ResponseEntity;
+use Throwable;
+
+class ResponseError
+{
+    /**
+     * response error.
+     * @param Throwable $throwable
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    public static function responseError(Throwable $throwable, ResponseInterface $response): ResponseInterface
+    {
+        $data = null;
+
+        if ((! $throwable instanceof ServiceException)
+            && (env('APP_ENV') === 'local' || env('APP_DEBUG') === true)) {
+            $data = (new ExceptionInfo($throwable))->getExceptionInfo();
+        }
+
+
+        if (is_string($throwable->getCode())) {
+            $errorCode = ResponseEntity::DEFAULT_STRING_CODE;
+        } else {
+            // 因 php 异常 code 默认为 0 与接口默认成功 code 为 0 冲突所以转换一下.
+            $errorCode = $throwable->getCode() === ResponseEntity::SUCCESS_CODE
+                ? ResponseEntity::DEFAULT_FAIL_CODE
+                : $throwable->getCode();
+        }
+
+        if ($throwable instanceof NondisclosureException) {
+            $message = ResponseEntity::DEFAULT_ERROR_MESSAGE;
+        } else {
+            // 若 code 为 string 时，则将 code 放 message 前面.
+            $message = is_string($throwable->getCode())
+                ? '[' . $throwable->getCode() . ']' . $throwable->getMessage()
+                : $throwable->getMessage();
+        }
+
+        $statusCode = $throwable instanceof ServiceException ? 200 : 500;
+
+        return $response
+            ->withStatus($statusCode)
+            ->withBody(new SwooleStream(Json::encode(ResponseEntityFactory::responseEntity(
+                $data,
+                null,
+                $message,
+                $errorCode
+            ))))->withHeader('Content-Type', 'application/json');
+    }
+}
